31e1e9d4064740eaeb47aa59395a61a03b5a0a60
Caleb McCann <mccanncm66@gmail.com>
2025-01-16 13:30:55 -0800

    Responsive work, added CardMemberInfo class, more modal work

---
+++ EBizChargeChallenge/Data/Models/CardMemberInfo.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace EBizChargeChallenge.Data.Models;
+
+public class CardMemberInfo
+{
+    public CreditCard MemberCreditCard { get; set; }
+    public string BillingAddress { get; set; }
+    public string NameOnCard { get; set; }
+    public string ZipCode { get; set; }
+    public string CardNickname { get; set; }
+    public int ExpireMonth { get; set; }
+    public int ExpireYear { get; set; }
+
+}
